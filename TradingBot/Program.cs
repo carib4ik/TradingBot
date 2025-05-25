4283edb6a8f68296d4bfe30b212b7fb10b1ad6b6
carib4ik <83468679+carib4ik@users.noreply.github.com>
2025-05-25 21:12:49 +0800

    Update Program.cs

--- TradingBot/Program.cs
+++ TradingBot/Program.cs
@@ -8,11 +8,11 @@ public static class Program
 {
     public static async Task Main(string[] args)
     {
-        var botClient = new TelegramBotClient("7892283240:AAEboODXlIAmpA3FzNvAFpAjXV_AQ4p_3eM");
+        var botClient = new TelegramBotClient("");
         
         var usersDataProvider = new UsersDataProvider();
         var marketDataService = new MarketDataService();
-        var chatGptService = new ChatGptService("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");
+        var chatGptService = new ChatGptService("");
         
         var chatStateMachine = new ChatStateMachine(botClient, usersDataProvider, chatGptService, marketDataService);
         var chatStateController = new ChatStateController(chatStateMachine);
